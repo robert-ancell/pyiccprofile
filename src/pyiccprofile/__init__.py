be1c0b85c61fc7fa7c14f46bb299eb3ab20c8c08
Robert Ancell <robert.ancell@gmail.com>
2026-07-28 23:49:42 +1200

    Expose more classes

--- src/pyiccprofile/__init__.py
+++ src/pyiccprofile/__init__.py
@@ -1,4 +1,16 @@
+from pyiccprofile.chromatic_adaptation import ICCChromaticAdaptation
 from pyiccprofile.codec import ICCDateTime
+from pyiccprofile.copyright import ICCCopyright
+from pyiccprofile.element import ICCTaggedElement, ICCUnknownTaggedElement
+from pyiccprofile.lut8 import ICCLut8
+from pyiccprofile.lut16 import ICCLut16
+from pyiccprofile.lut_atob import ICCLutAToB
+from pyiccprofile.lut_btoa import ICCLutBToA
+from pyiccprofile.media_white_point import ICCMediaWhitePoint
+from pyiccprofile.multi_localized_unicode_type import ICCMultiLocalizedUnicodeType
+from pyiccprofile.perceptual_rendering_intent_gamut import (
+    ICCPerceptualRenderingIntentGamut,
+)
 from pyiccprofile.profile import (
     ICCColorSpace,
     ICCPrimaryPlatform,
@@ -9,11 +21,22 @@ from pyiccprofile.profile import (
 from pyiccprofile.profile_description import ICCProfileDescription
 
 __all__ = [
+    "ICCChromaticAdaptation",
     "ICCColorSpace",
+    "ICCCopyright",
     "ICCDateTime",
+    "ICCLut8",
+    "ICCLut16",
+    "ICCLutAToB",
+    "ICCLutBToA",
+    "ICCMediaWhitePoint",
+    "ICCMultiLocalizedUnicodeType",
+    "ICCPerceptualRenderingIntentGamut",
     "ICCPrimaryPlatform",
     "ICCProfile",
     "ICCProfileClass",
     "ICCProfileDescription",
     "ICCRenderingIntent",
+    "ICCTaggedElement",
+    "ICCUnknownTaggedElement",
 ]
