9a39e445ae0a619f83c073293334884408b93c45
Robert Ancell <robert.ancell@gmail.com>
2026-07-28 23:41:25 +1200

    Fix tests

--- tests/test_icc_profile.py
+++ tests/test_icc_profile.py
@@ -1,14 +1,14 @@
 import pyiccprofile
+from pyiccprofile.profile import ICCRenderingIntent
 
 
 def test_icc_profile():
     profile = pyiccprofile.ICCProfile(
-        preferred_cmm_type=0,
         profile_class=pyiccprofile.ICCProfileClass.INPUT,
-        data_color_space=pyiccprofile.ICCDataColorSpace.CIELAB,
-        pcs=pyiccprofile.ICCDataColorSpace.CIELAB,
+        data_color_space=pyiccprofile.ICCColorSpace.LAB,
+        pcs=pyiccprofile.ICCColorSpace.LAB,
         creation_time=pyiccprofile.ICCDateTime(2026, 7, 26, 16, 18, 42),
-        rendering_intent=0,
+        rendering_intent=ICCRenderingIntent.PERCEPTUAL,
         tagged_elements=[],
     )
     encoded_profile = profile.encode()
